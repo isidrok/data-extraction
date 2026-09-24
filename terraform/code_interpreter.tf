d1e67516c86c28e0cbd140fe704f47731c3ce9da
Isidro Torregrosa <isistorre95@gmail.com>
2026-09-24 13:03:33 +0200

    code interpreter and s3

---
+++ terraform/code_interpreter.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_role" "code_interpreter" {
+  name = "${var.project}-code-interpreter"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "bedrock.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "code_interpreter_s3" {
+  name = "s3-get-object"
+  role = aws_iam_role.code_interpreter.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "s3:GetObject"
+      Resource = "${aws_s3_bucket.data.arn}/*"
+    }]
+  })
+}
+
+resource "aws_bedrockagentcore_code_interpreter" "main" {
+  name        = "${var.project}-code-interpreter"
+  description = "Code interpreter sandbox for data extraction"
+
+  execution_role_arn = aws_iam_role.code_interpreter.arn
+
+  network_configuration {
+    network_mode = "SANDBOX"
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,11 @@ output "github_actions_role_arn" {
   description = "ARN to use in the GitHub Actions workflow's role-to-assume"
   value       = aws_iam_role.github_actions.arn
 }
+
+output "code_interpreter_arn" {
+  value = aws_bedrockagentcore_code_interpreter.main.code_interpreter_arn
+}
+
+output "data_bucket_name" {
+  value = aws_s3_bucket.data.bucket
+}
